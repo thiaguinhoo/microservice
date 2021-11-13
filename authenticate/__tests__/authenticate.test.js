fc8305617ebe4abe09929f71f6496b3882b41288
thiaguinhoo <thiagoluizsog@gmail.com>
2021-11-13 13:31:21 -0300

    added first test

---
+++ authenticate/__tests__/authenticate.test.js
@@ -0,0 +1,11 @@
+const supertest = require('supertest')
+const application = require('../src/application')
+
+const request = supertest(application)
+
+describe('authenticate endpoint', () => {
+  it('should return status code 401', async () => {
+    const response = await request.get('/message');
+    expect(response.status).toBe(401)
+  })
+})
